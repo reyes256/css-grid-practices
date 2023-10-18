584694db814a33f413254af3cac71a88f6abac87
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-10-17 18:47:13 -0700

    Added custom index page
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="body bg-gray-400">
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.body {
+  height: 100dvh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="body p-4">
+    <h1 class="py-4 text-xl font-bold">Lista de Practicas</h1>
+    <div class="grid grid-cols-3 gap-4">
+      <div class="col-span-1 grid gap-4">
+        <NuxtLink
+          to="/basics"
+          class="rounded-md bg-gray-100 p-4 hover:cursor-pointer hover:bg-gray-200"
+          >Uso Basico</NuxtLink
+        >
+        <!-- <NuxtLink
+          to="/"
+          class="rounded-md bg-gray-100 p-4 hover:cursor-pointer hover:bg-gray-200"
+          >multi-column</NuxtLink
+        >
+        <NuxtLink
+          to=""
+          class="rounded-md bg-gray-100 p-4 hover:cursor-pointer hover:bg-gray-200"
+          >holy-grail</NuxtLink
+        >
+        <NuxtLink
+          to=""
+          class="rounded-md bg-gray-100 p-4 hover:cursor-pointer hover:bg-gray-200"
+          >masonry</NuxtLink
+        >
+        <NuxtLink
+          to=""
+          class="rounded-md bg-gray-100 p-4 hover:cursor-pointer hover:bg-gray-200"
+          >portfolio</NuxtLink
+        >
+        <NuxtLink
+          to=""
+          class="rounded-md bg-gray-100 p-4 hover:cursor-pointer hover:bg-gray-200"
+          >twelve-col</NuxtLink
+        > -->
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
